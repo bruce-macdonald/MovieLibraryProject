05ba97ba9275a763c536150a023dd5402fda8619
larutow <lucas.rutowski@gmail.com>
2020-11-30 13:55:08 -0600

    added additional details to cards. added collapsable edit menu

--- Client/app.js
+++ Client/app.js
@@ -15,11 +15,11 @@
             data: JSON.stringify(dict),
             success: function (data, textStatus, jQxhr) {
                 $('#response pre').html("Movie successfully added - Title: " + data.title + " - Director: " + data.director + " - Genre: " + data.genre);
-                $(`#movieCards`).prepend(`<div class = 'card' style = 'width: 18rem;'>
+                $(`#movieCards`).prepend(`<div class = 'card' style = 'width: 14rem;'>
                 <img class = 'card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1280px-SNice.svg.png'alt = 'smiley face'>
                 <div class = 'card-body'> <h5 class = 'card-title'>${data.title}</h5>
+                <a href='#' class='btn btn-primary' id='${data.movieId}-button'>Details / Edit</a>
                 <p class= 'card-text'>ABC</p>
-                <a href='#' class='btn btn-primary'>Go somewhere</a>
                 </div>
                 </div>`
                 )
@@ -42,7 +42,29 @@
             contentType: 'application/json',
             success: function (data, textStatus, jQxhr) {
                 data.forEach(movie => {
-                    $(`#movieCards`).prepend(`<div class = 'card' style = 'width: 18rem;'><img class = 'card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1280px-SNice.svg.png'alt = 'smiley face'><div class = 'card-body'> <h5 class = 'card-title'>${movie.title}</h5> <p class= 'card-text'>ABC</p><a href='#' class='btn btn-primary'>Go somewhere</a></div></div>`)
+                    $(`#movieCards`).prepend(`
+                    <div class = 'card m-2' style = 'width: 14rem;'>
+                        <img class = 'card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1280px-SNice.svg.png'alt = 'smiley face'>
+                            <div class = 'card-body'>
+                            <h5 class = 'card-title'>${movie.title}</h5>
+                            <ul class='list-group list-group-flush'>
+                                <li class='list-group-item'>Director: ${movie.director}</li>
+                                <li class='list-group-item'>Genre: ${movie.genre}</li>
+                            </ul>
+                            <a href='#${movie.movieId}Collapse' class='btn btn-primary mt-2' data-toggle='collapse' aria-expanded='false' aria-controls='collapseExample' role='button' id='${movie.movieId}-button'>Edit</a>
+                            
+                            <div id='${movie.movieId}Collapse' class='collapse'>
+                            <form id="${movie.movieId}-form">
+                            <input type="text" name="title" placeholder="${movie.title}" />
+                            <input type="text" name="director" placeholder="${movie.director}" />
+                            <input type="text" name="genre" placeholder="${movie.genre}" />
+
+                            <button type="submit">Submit</button>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                    `)
 
                 });
             },
@@ -51,15 +73,15 @@
             }
         });
 
-        // e.preventDefault();
+    }
+
+
+
+    function getMovieDetails() {
+        $.ajax({
+
+        })
     }
 
 }
 )(jQuery);
-
-// (function($){
-
-
-
-// }
-// )(jQuery);
