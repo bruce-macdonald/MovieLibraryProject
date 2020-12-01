1b4e07932820e269e5f56965fd9f6f7453ca90d8
unknown <meekhunsutp@gmail.com>
2020-12-01 08:49:30 -0600

    Proofed app.js for consistency

--- Client/app.js
+++ Client/app.js
@@ -15,15 +15,15 @@ let movieArray = [];
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function (data, textStatus, jQxhr) {
-                $('#response pre').html("Movie successfully added - Title: " + data.title + " - Director: " + data.director + " - Genre: " + data.genre);
-                $(`#movieCards`).prepend(`<div class = 'card' style = 'width: 14rem;'>
-                <img class = 'card-img-top' src='${data.movieImage}' alt = 'movie poster'>
-                <div class = 'card-body'> <h5 class = 'card-title'>${data.title}</h5>
-                <a href='#' class='btn btn-primary' id='${data.movieId}-button'>Details / Edit</a>
-                <p class= 'card-text'>ABC</p>
-                </div>
-                </div>`
-                )
+                $('#response pre').html('Movie successfully added - Title: ' + data.title + ' - Director: ' + data.director + ' - Genre: ' + data.genre);
+                $(`#movieCards`).prepend(`
+                <div class = 'card' style = 'width: 14rem;'>
+                    <img class = 'card-img-top' src='${data.movieImage}' alt = 'movie poster'>
+                    <div class = 'card-body'> <h5 class = 'card-title'>${data.title}</h5>
+                        <a href='#' class='btn btn-primary' id='${data.movieId}-button'>Details / Edit</a>
+                        <p class= 'card-text'>ABC</p>
+                    </div>
+                </div>`)
             },
             error: function (jqXhr, textStatus, errorThrown) {
                 console.log(errorThrown);
@@ -46,33 +46,28 @@ let movieArray = [];
                     $(`#movieCards`).prepend(`
                     <div class = 'card m-2' style = 'width: 14rem;'>
                         <img class = 'card-img-top' src='${movie.movieImage}' alt = 'movieposter'>
-                            <div class = 'card-body'>
+                        <div class = 'card-body'>
                             <h5 class = 'card-title'>${movie.title}</h5>
                             <ul class='list-group list-group-flush'>
                                 <li class='list-group-item'>Director: ${movie.director}</li>
                                 <li class='list-group-item'>Genre: ${movie.genre}</li>
                             </ul>
                             <a href='#${movie.movieId}Collapse' class='btn btn-primary mt-2' data-toggle='collapse' aria-expanded='false' aria-controls='collapseExample' role='button' id='${movie.movieId}-button'>Edit</a>
-                            
                             <div id='${movie.movieId}Collapse' class='collapse'>
-                            
-                            <div class='form-group mt-3'>
-                            <label for='${movie.title}Input'>Movie Title</label>
-                            <input type="text" name="title" id='title-${movie.movieId}' value="${movie.title}" />
-                            <br>
-                            <label for='${movie.director}Input' class='mt-2'>Director</label>
-                            <input type="text" name="director" id='director-${movie.movieId}' value="${movie.director}" />
-                            <br>
-                            <label for='${movie.genre}Input' class='mt-2'>Genre</label>
-                            <input type="text" name="genre" id='genre-${movie.movieId}' value="${movie.genre}" />
-
-                            <button onclick='editMovieDetails(${movie.movieId})'  class='btn btn-danger mt-3'>Confirm Change</button>
-                            </div>
+                                <div class='form-group mt-3'>
+                                    <label for='${movie.title}Input'>Movie Title</label>
+                                    <input type='text' name="title" id='title-${movie.movieId}' value='${movie.title}' />
+                                    <label for='${movie.director}Input' class='mt-2'>Director</label>
+                                    <input type='text' name='director' id='director-${movie.movieId}' value='${movie.director}' />
+                                    <label for='${movie.genre}Input' class='mt-2'>Genre</label>
+                                    <input type='text' name='genre' id='genre-${movie.movieId}' value='${movie.genre}' />
+                                    <label for='${movie.movieImage}Input' class='mt-2'>Movie Poster</label>
+                                    <input type='text' name='movieImage' id='movieImage-${movie.movieId}' value='${movie.movieImage}' />
+                                    <button onclick='editMovieDetails(${movie.movieId})'  class='btn btn-danger mt-3'>Confirm Change</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    `)
-                    
+                    </div>`)
                 });
             movieArray = data;
             },
@@ -80,9 +75,7 @@ let movieArray = [];
                 console.log(errorThrown);
             }
         });
-
     }
-
 }
 )(jQuery);
 
@@ -93,7 +86,8 @@ function editMovieDetails(id) {
         MovieId: id,
         Title: $('#title-'+id).val(),
         Director: $('#director-'+id).val(),
-        Genre: $('#genre-'+id).val()
+        Genre: $('#genre-'+id).val(),
+        MovieImage: $('#movieImage-'+id).val()
     };
     var putUrl = 'https://localhost:44325/api/movie/';
     console.log(dict);
@@ -106,11 +100,9 @@ function editMovieDetails(id) {
         data: JSON.stringify(dict),
         success: function (data, textStatus, jQxhr) {
             location.reload();
-
         },
         error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
         }
     });
-
 }
